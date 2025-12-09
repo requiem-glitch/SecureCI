f5040234d83ccedf66a99bb7fc4a76be06091cf1
requiem-glitch <63611193+requiem-glitch@users.noreply.github.com>
2025-12-09 10:50:06 +0300

    Create run-docker-bench.sh

---
+++ scripts/run-docker-bench.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Запуск Docker Bench Security..."
+docker run --rm --net host --pid host --cap-add audit_control \
+    -v /etc:/etc:ro \
+    -v /usr/bin/containerd:/usr/bin/containerd:ro \
+    -v /usr/bin/runc:/usr/bin/runc:ro \
+    -v /usr/lib/systemd:/usr/lib/systemd:ro \
+    -v /var/lib:/var/lib:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    docker/docker-bench-security
